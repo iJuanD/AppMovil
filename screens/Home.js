04525d280846bec944f08589affa1d8816f8a6f5
IJuanDHerrera <105659960+iJuanD@users.noreply.github.com>
2022-05-21 07:38:06 -0500

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Este es el Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/Search.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function  Search() {
+  return (
+    <View>
+      <Text>Este es el Search</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
